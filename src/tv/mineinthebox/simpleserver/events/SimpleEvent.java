9544b77c39fbd3d6bcfc42f7d3b3da5a8c7cd8d9
xize <glucassen@live.nl>
2015-01-15 17:34:21 +0100

    first initial commit

---
+++ src/tv/mineinthebox/simpleserver/events/SimpleEvent.java
@@ -0,0 +1,49 @@
+package tv.mineinthebox.simpleserver.events;
+
+import java.io.File;
+import java.util.Map;
+
+import tv.mineinthebox.simpleserver.Content;
+import tv.mineinthebox.simpleserver.HttpClient;
+import tv.mineinthebox.simpleserver.MimeType;
+import tv.mineinthebox.simpleserver.Server;
+
+public interface SimpleEvent {
+	
+	public boolean isGetRequest();
+	
+	public boolean isPostRequest();
+	
+	public Server getServer();
+	
+	public String getUri();
+
+	public void addContent(byte[] bytes);
+	
+	public void addContent(Content content);
+	
+	public void setContent(File f);
+	
+	public void setContent(byte[] bytes);
+	
+	public void setContent(Content content);
+	
+	public boolean hasContents();
+	
+	public byte[] getContents();
+	
+	public boolean isCancelled();
+	
+	public void setCancelled(boolean bol);
+	
+	public MimeType getMimeType();
+	
+	public void setMimeType(MimeType type);
+	
+	public boolean hasFormData();
+	
+	public Map<String, String> getFormData();
+	
+	public HttpClient getClientHeaderData();
+	
+}
